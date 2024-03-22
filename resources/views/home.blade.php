339c216ac5da996a850f579f8de09ff47c666d50
= <=>
2024-03-22 09:18:44 -0300

    carrosel bootstrap ok

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -6,6 +6,49 @@
 
 @section('content')
     <div class="container">
+        <div class="text-center my-3">
+            <span class="btn btn-outline-dark fs-3">AGENDE SEU CORTE</span>
+        </div>
+    </div>
+    <div class="container">
+        <div class="fs-1">
+            GALERIA
+        </div>
+        <div id="carouselExampleAutoplaying" class="carousel slide" data-bs-ride="carousel">
+        <div class="carousel-inner" style="height: 65vh">
+            <div class="carousel-item active">
+            <img src="storage/corte_1.jpeg" class="d-block w-100" alt="corte_1">
+            </div>
+            <div class="carousel-item">
+            <img src="storage/corte_2.jpg" class="d-block w-100" alt="corte_2">
+            </div>
+            <div class="carousel-item">
+            <img src="storage/corte_3.jpeg" class="d-block w-100" alt="corte_3">
+            </div>
+        </div>
+        <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleAutoplaying" data-bs-slide="prev">
+            <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+            <span class="visually-hidden">Previous</span>
+        </button>
+        <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleAutoplaying" data-bs-slide="next">
+            <span class="carousel-control-next-icon" aria-hidden="true"></span>
+            <span class="visually-hidden">Next</span>
+        </button>
+        </div>
+    </div>
+    <div class="container">
+        <div class="fs-1">
+            Início
+        </div>
+        <p class="my-5">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Numquam tempora ea, ullam doloribus ipsam ut ipsum error ratione adipisci debitis eveniet, expedita accusantium, quidem doloremque ad enim necessitatibus illum cum.
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Numquam tempora ea, ullam doloribus ipsam ut ipsum error ratione adipisci debitis eveniet, expedita accusantium, quidem doloremque ad enim necessitatibus illum cum.
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Numquam tempora ea, ullam doloribus ipsam ut ipsum error ratione adipisci debitis eveniet, expedita accusantium, quidem doloremque ad enim necessitatibus illum cum.
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Numquam tempora ea, ullam doloribus ipsam ut ipsum error ratione adipisci debitis eveniet, expedita accusantium, quidem doloremque ad enim necessitatibus illum cum.
+
+        </p>
+    </div>
+    <div class="container">
         <div class="fs-1">
             Início
         </div>
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -55,55 +55,40 @@
                     <button type="button" class="btn-close" data-bs-dismiss="offcanvas" aria-label="Close"></button>
                 </div>
                 <div class="offcanvas-body">
-                    teste
-                </div>
-            </div>
-        </div>
-        
-
-
-                {{-- <div class="collapse" id="navbarSupportedContent">
-                    
-
-                    <!-- Right Side Of Navbar -->
+                    <hr/>
                     <ul class="navbar-nav ms-auto">
+                        <li class="nav-item">
+                            <a class="nav-link" href="/">{{ __('Início') }}</a>
+                        </li>
                         <!-- Authentication Links -->
                         @guest
                             @if (Route::has('login'))
                                 <li class="nav-item">
-                                    <a class="nav-link" href="{{ route('login') }}">{{ __('Login') }}</a>
+                                    <a class="nav-link" href="{{ route('login') }}">{{ __('Entrar') }}</a>
                                 </li>
                             @endif
 
                             @if (Route::has('register'))
                                 <li class="nav-item">
-                                    <a class="nav-link" href="{{ route('register') }}">{{ __('Register') }}</a>
+                                    <a class="nav-link" href="{{ route('register') }}">{{ __('Cadastrar-se') }}</a>
                                 </li>
                             @endif
                         @else
-                            <li class="nav-item dropdown">
-                                <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
-                                    {{ Auth::user()->name }}
+                            <p class="fs-4">Bem-vindo {{Auth()->user()->name}}</p>
+                            <li class="nav-item">
+                                <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
+                                    @csrf
+                                </form>
+                                <a class="dropdown-item" href="{{ route('logout') }}" onclick="event.preventDefault(); document.getElementById('logout-form').submit();">
+                                    {{ __('Sair') }}
                                 </a>
-
-                                <div class="dropdown-menu dropdown-menu-end" aria-labelledby="navbarDropdown">
-                                    <a class="dropdown-item" href="{{ route('logout') }}"
-                                    onclick="event.preventDefault();
-                                                    document.getElementById('logout-form').submit();">
-                                        {{ __('Logout') }}
-                                    </a>
-
-                                    <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
-                                        @csrf
-                                    </form>
-                                </div>
                             </li>
                         @endguest
                     </ul>
                 </div>
             </div>
-        </nav> --}}
-
+        </div>
+        
         <main class="my-4">
             @yield('content')
         </main>
